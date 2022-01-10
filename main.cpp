937265642df34481609bbd1afd9c72ec9f0676c7
sbrzeski100 <90987467+anatoliy2137@users.noreply.github.com>
2022-01-10 18:22:02 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <cstdlib>
+#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+using namespace std;
+int main(int argc, char *argv[])
+{
+ for ( int brz=1;brz<=22;brz+=1)
+{ 
+ cout<<brz<<' ';
+}
+ cout<<endl;
+ system("PAUSE");
+ return EXIT_SUCCESS;
+}
