b637df1ff39202c8397ab6ec06e40b2c2f59058f
sbrzeski100 <90987467+anatoliy2137@users.noreply.github.com>
2022-01-18 09:17:40 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,15 +1,18 @@
 #include <cstdlib>
-#include <iostream>
-#include <stdio.h>
-#include <conio.h>
-using namespace std;
-int main(int argc, char *argv[])
-{
- for ( int brz=1;brz<=22;brz+=1)
-{ 
- cout<<brz<<' ';
-}
- cout<<endl;
- system("PAUSE");
- return EXIT_SUCCESS;
-}
+ #include <iostream>
+  #include <stdio.h>
+   #include <conio.h>
+    using namespace std; int main(int argc, char *argv[])
+     { 
+          for ( int i=13.5;i<=202;i++) 
+          { 
+              if((i) % 20==0)
+              { system("PAUSE");system("cls");}
+              cout<<"kod znaku  "<<i<<"   "<<(char)i<<"\n"; 
+          }
+          
+          
+           cout<<endl; 
+           system("PAUSE");
+            return EXIT_SUCCESS; 
+            }
